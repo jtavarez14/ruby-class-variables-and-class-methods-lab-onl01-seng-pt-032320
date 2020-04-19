dbceb678560870aebb0bfcc0890c7c79051805d1
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-19 07:34:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,35 @@
+require "pry"
+class Song 
+  attr_accessor :name, :artist, :genre
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name 
+    @artist = artist
+    @genre = genre
+    @@artists << @artist
+    @@genres << @genre
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.artists 
+    @@artists.uniq
+  end 
+  
+  def self.genres 
+    @@genres.uniq
+  end 
+  
+  def self.genre_count 
+    unique_genre = @@genres.uniq
+    genre_count = unique_genre.count
+    genre_numbers = Hash[@@genres.collect {|one_genre| [one_genre, one_genre.count]}]
+  end 
+    end
+    
+  
