a8bf1f6e063cb59b1a9088e7ea5682f60fb9c116
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-19 21:38:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -25,12 +25,10 @@ class Song
     @@genres.uniq
   end 
   
-  def self.genre_count 
-    unique_genre = @@genres.uniq
-    genre_count = unique_genre.count
-    genre_hash = {}
-    binding.pry
+ def self.genre_count
+    @@genres.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end 
-    end
+  
+end
     
   
